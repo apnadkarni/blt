323e5e5422057d7ac2e2a3a4b2eaab6e87f4f2e0
gah <gahowlett@gmail.com>
2015-06-08 12:05:58 -0400

    reorganize demos

--- demos/picture/circle.tcl
+++ demos/picture/circle.tcl
@@ -1,6 +1,6 @@
 package require BLT
 
-set img [image create picture -file images/blt98.gif]
+set img [image create picture -file ../images/blt98.gif]
 set brush [blt::paintbrush create tile -image $img -opacity 50]
 
 set s1 [image create picture -width 25 -height 25]
@@ -11,7 +11,6 @@ $s1 draw circle 12 12 5 -shadow 0 -linewidth 1 \
 set bg [image create picture -width 600 -height 600]
 $bg blank white
 
-
 set n 500
 puts stderr [time {
     blt::vector create points -length $n
@@ -21,8 +20,6 @@ puts stderr [time {
 
 puts stderr [time {
     foreach {x y} [points values] {
-	#puts stderr "x=$x y=$y"
-	#$bg copy $s1 -to "$x $y" -blend yes
 	$bg draw circle [expr int($x)] [expr int($y)] 20 \
 	    -shadow 0 -linewidth 0 \
 	    -color $brush -antialias no
@@ -31,4 +28,3 @@ puts stderr [time {
 label .l -image $bg
 pack .l
 
-$bg export ps -file picture2.ps
