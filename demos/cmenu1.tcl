0c6f57e0117473585a0e3fc29dddd85946c4e988
gah <gahowlett@gmail.com>
2017-08-08 07:47:57 -0500

    fix add chevron

--- demos/cmenu1.tcl
+++ demos/cmenu1.tcl
@@ -42,7 +42,7 @@ blt::combomenu .e.m  \
     -yscrollbar .e.m.ybar \
     -xscrollbar .e.m.xbar
 blt::tk::scrollbar .e.m.xbar -style tk
-blt::tk::scrollbar .e.m.ybar -style xp
+blt::tk::scrollbar .e.m.ybar -style xp -arrowcolor grey40
 .e.m sort configure -auto 1
 
 set onOff 1
