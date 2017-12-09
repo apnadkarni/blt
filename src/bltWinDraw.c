707caf05709c17c1d18fc3c54b2c6004c82b7347
gah <gahowlett@gmail.com>
2017-12-08 19:23:50 -0600

    fix for clipping

--- src/bltWinDraw.c
+++ src/bltWinDraw.c
@@ -1906,7 +1906,7 @@ Blt_EmulateXFillRectangle(Display *display, Drawable drawable, GC gc,
             hBrush = CreateSolidBrush(gc->foreground);
             hBitmap = CreateCompatibleBitmap(hDC, w, h);
             hOldBitmap = SelectBitmap(hMemDC, hBitmap);
-            FillRect(memDC, &, hBrush);
+            FillRect(hMemDC, &, hBrush);
             BitBlt(hDC, x, y, w, h, hMemDC, 0, 0, SRCCOPY);
             SelectObject(hMemDC, hOldBitmap);
             DeleteBitmap(hBitmap);
